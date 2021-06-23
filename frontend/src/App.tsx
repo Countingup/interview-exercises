2951b693d1c8164c7a0585c8a74a49f4c551f078
Patrick Curry <pwfcurry@users.noreply.github.com>
2021-06-23 16:47:25 +0100

    Removed rendering test, added file reading example (#4)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
 import Table from "./Table";
+import data from "./out.json";
 
 const App = () => {
+  console.log(data);
+
   return (
     <div className="App">
       <h1>Countingup</h1>
