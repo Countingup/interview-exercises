ab7d1ee34196a8f94e7fe2667ba29b69d10e3d7c
Patrick Curry <patrick.curry@countingup.com>
2021-06-23 16:22:57 +0100

    Removed rendering test, added file reading example

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import data from "./out.json";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("should read data", () => {
+  expect(data).toHaveLength(11);
 });
